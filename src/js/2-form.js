f196c8ad690643d6c6714ff446290a74b697944d
Andrii D. <and05gt@gmail.com>
2024-07-10 23:26:00 +0200

    Add form script

---
+++ src/js/2-form.js
@@ -0,0 +1,42 @@
+let formData = {
+  email: '',
+  message: '',
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', handleFormInput);
+form.addEventListener('submit', handleFormSubmit);
+
+formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+const { email, message } = form.elements;
+
+populateForm();
+
+function handleFormInput(event) {
+  formData = { email: email.value.trim(), message: message.value.trim() };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateForm() {
+  if (formData) {
+    email.value = formData.email || '';
+    message.value = formData.message || '';
+  }
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  event.currentTarget.reset();
+  formData = {};
+}
